f78b2c3f54baa96af760e916b4cb45cf316896c4
Edwin Moreno <edwinmoreno903@gmail.com>
2026-05-03 15:52:04 -0500

    Arreglando Carrito de Compras

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
+import { useState } from "react";
+import Guitar from "./components/Guitar";
+import Header from "./components/Header";
+import { db } from "./data/db";
+
 function App() {
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {    
+
+    const itemExists = cart.findIndex(guitar => guitar.id === item.id)
+    
+    if (itemExists >= 0) return
+    
+
+    setCart([...cart, item])
+    
+  }
 
   return (
     <>
-      <h1>Guitar La</h1>
+      <Header />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar guitar={guitar} key={guitar.id} addToCart={addToCart} />
+          ))}
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
